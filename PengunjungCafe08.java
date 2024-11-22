9b795fd4fd8c9b3b2be11c6a9097f334fd4d1b54
ddickyy <dikiidarmawam05x2@gmail.com>
2024-11-22 13:55:29 +0700

    Selesai

---
+++ PengunjungCafe08.java
@@ -0,0 +1,13 @@
+public class PengunjungCafe08 {
+
+    public static void main(String[] args) {
+    daftarPengunjung("Ali", "Budi", "Citra");    
+    }
+
+    public static void daftarPengunjung (String... namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung");
+        for (String pengunjung : namaPengunjung) {
+            System.out.println("-" + pengunjung);
+        }
+    }
+}
