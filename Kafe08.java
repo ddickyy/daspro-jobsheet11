60acd59ef5786ff423a08664ed5b15641e7d1816
ddickyy <dikiidarmawam05x2@gmail.com>
2024-11-22 12:39:33 +0700

    Setelah soal

--- Kafe08.java
+++ Kafe08.java
@@ -1,9 +1,7 @@
 public class Kafe08 {
 
     public static void main(String[] args) {
-        Menu();
-    }
-    public static void Menu () {
+
         System.out.println("=====MENU RESTO KAFE=====");
         System.out.println("1. Kopi Hitam - Rp15,000");
         System.out.println("2. Cappuccino - Rp20,000");
@@ -13,6 +11,6 @@ public class Kafe08 {
         System.out.println("6. Mie Goreng - Rp18,000");
         System.out.println("==========================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
-    
     }
+
 }
